fa695ddb29efd7be679a37d20e257e233228345e
OyaCanli <oyacan@gmail.com>
2020-02-06 12:32:58 +0100

    change location entry

--- app/src/main/java/com/canli/oya/traininventoryroom/utils/BindingUtils.kt
+++ app/src/main/java/com/canli/oya/traininventoryroom/utils/BindingUtils.kt
@@ -5,13 +5,3 @@ package com.canli.oya.traininventoryroom.utils
 fun encloseInParenthesis(category: String?): String? {
     return category?.let {"($it)"}
 }
-
-fun attachLocationString(locationRow: String?, locationColumn : String?) : String? {
-    val row = locationRow ?: ""
-    val column = locationColumn ?: ""
-    return if(row.isNotBlank() && column.isNotBlank()){
-        "$row - $column"
-    } else {
-        "$row $column"
-    }
-}
