dbbb0c2ac93f140eb6038d1c0168c24ad0ce1e6a
OyaCanli <oyacan@gmail.com>
2021-09-09 16:53:00 +0200

    Update proguard rules

--- core/src/main/java/com/canlioya/core/models/Brand.kt
+++ core/src/main/java/com/canlioya/core/models/Brand.kt
@@ -1,7 +1,11 @@
 package com.canlioya.core.models
 
+import androidx.annotation.Keep
+
+@Keep
 data class Brand(
     var brandId: Int = 0,
     var brandName: String,
     var brandLogoUri: String? = null,
-    var webUrl: String? = null)
+    var webUrl: String? = null
+)
